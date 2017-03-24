a5627dd24d311a2476ecdc3ae474419ef7fc9aec
Vadim Markovtsev <vadim@sourced.tech>
2017-03-24 18:11:16 +0100

    Add smth which is able to train

---
+++ infer.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+
+from keras import models
+
+from tokens import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", required=True)
+    parser.add_argument("--diversity", type=float, default=1.0)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    model = models.load_model(args.model)
+    maxlen = model.inputs[0].shape[0]
+    x = numpy.zeros((maxlen, len(token_map)))
+    for line in sys.stdin:
+        ctx = eval(line)
+        x[:] = 0
+        for i in range(maxlen):
+            k = len(ctx) - maxlen + i
+            if k >= 0:
+                x[i] = token_map[ctx[k]]
+        pred = prediction2token(model.predict(x, verbose=0)[0], args.diversity)
+        sys.stdout.write("%r\n" % pred)
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ train.py
@@ -0,0 +1,93 @@
+import argparse
+import os
+import pickle
+import sys
+
+from keras import models, layers
+
+from tokens import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--maxlines", type=int, default=0)
+    parser.add_argument("--maxlen", type=int, default=100)
+    parser.add_argument("--start-offset", type=int, default=4)
+    parser.add_argument("--neurons", type=int, default=128)
+    parser.add_argument("--type", default="LSTM")
+    parser.add_argument("--dropout", type=float, default=0)
+    parser.add_argument("--recurrent-dropout", type=float, default=0)
+    parser.add_argument("--activation", default="sigmoid")
+    parser.add_argument("--optimizer", default="adam")
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--epochs", type=int, default=50)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    maxlen = args.maxlen
+    maxlines = args.maxlines
+    start_offset = args.start_offset
+
+    if os.path.exists(args.input + ".pickle"):
+        print("loading the cached dataset...")
+        with open(args.input + ".pickle", "rb") as fin:
+            x, y = pickle.load(fin)
+    else:
+        x = []
+        y = []
+        with open(args.input) as fin:
+            for lineno, line in enumerate(fin):
+                if lineno % 1000 == 0:
+                    print("line #%d" % lineno)
+                if lineno > maxlines:
+                    break
+                ctx = eval(line)
+                for i in range(start_offset, len(ctx)):
+                    sample = numpy.zeros((maxlen, len(token_map)),
+                                         dtype=numpy.float32)
+                    for j in range(maxlen):
+                        k = i - maxlen + j
+                        if k >= 0:
+                            sample[j] = token_map[ctx[k]]
+                    x.append(sample)
+                    y.append(token_map[ctx[i]])
+        x = numpy.array(x, dtype=numpy.float32)
+        y = numpy.array(y, dtype=numpy.float32)
+        print("saving the cache...")
+        with open(args.input + ".pickle", "wb") as fout:
+            pickle.dump((x, y), fout, protocol=-1)
+    print("x:", x.shape)
+    print("y:", y.shape)
+    model = train(x, y, **args.__dict__)
+    model.save(args.output, overwrite=True)
+
+
+def train(x, y, **kwargs):
+    neurons = kwargs.get("neurons", 128)
+    dropout = kwargs.get("dropout", 0)
+    recurrent_dropout = kwargs.get("recurrent_dropout", 0)
+    activation = kwargs.get("activation", "sigmoid")
+    optimizer = kwargs.get("optimizer", "adam")
+    batch_size = kwargs.get("batch_size", 128)
+    epochs = kwargs.get("epochs", 50)
+    layer_type = kwargs.get("type", "LSTM")
+    model = models.Sequential()
+    model.add(getattr(layers, layer_type)(
+        neurons, dropout=dropout, recurrent_dropout=recurrent_dropout,
+        input_shape=x[0].shape, return_sequences=True))
+    model.add(getattr(layers, layer_type)(
+        neurons // 2, dropout=dropout, recurrent_dropout=recurrent_dropout,
+        input_shape=x[0].shape))
+    model.add(layers.Dense(x[0].shape[-1], activation=activation))
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=optimizer, metrics=["accuracy"])
+    model.fit(x, y, batch_size=batch_size, epochs=epochs)
+    return model
+
+
+if __name__ == "__main__":
+    sys.exit(main())
