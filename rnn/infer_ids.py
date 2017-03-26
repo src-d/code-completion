04a966ba83f06c747c2d244fd9275ce3c46b41c8
Vadim Markovtsev <vadim@sourced.tech>
2017-03-26 12:33:15 +0200

    Make ids inference usable

--- rnn/infer_ids.py
+++ rnn/infer_ids.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import pickle
 import sys
 
 os.putenv("TF_CPP_MIN_LOG_LEVEL", os.getenv("TF_CPP_MIN_LOG_LEVEL", "2"))
@@ -11,30 +12,46 @@ with open("/dev/null", "w") as devnull:
     del stderr
 
 from tokens import *
+from train_ids import extract_names
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", required=True)
-    parser.add_argument("--number", type=int, default=1)
+    parser.add_argument("--number", type=int, default=5)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     model = models.load_model(args.model)
+    with open(args.model + ".voc", "rb") as fin:
+        vocabulary = pickle.load(fin)
+    ivoc = [None] * len(vocabulary)
+    for key, val in vocabulary.items():
+        ivoc[val] = key
     maxlen = model.inputs[0].shape[1].value
-    x = numpy.zeros((1, maxlen, len(token_map)))
+    x = numpy.zeros((1, maxlen, len(vocabulary)))
     for line in sys.stdin:
         ctx = eval(line)
         x[:] = 0
-        for i in range(maxlen):
-            k = len(ctx) - maxlen + i
-            if k >= 0:
-                x[0, i] = token_map[ctx[k]]
-
-        preds = prediction2token(model.predict(x, verbose=0)[0], args.number)
-        sys.stdout.write("%s\n" % " ".join("%r@%.3f" % p for p in preds))
+        word = False
+        words = []
+        for c in ctx:
+            if c == ID_S:
+                word = True
+            elif word:
+                word = False
+                wadd = tuple(vocabulary[p] for p in extract_names(c))
+                if wadd:
+                    words.append(wadd)
+        for i, w in enumerate(words):
+            for c in w:
+                x[0, maxlen - len(words) + i, c] = 1
+        preds = model.predict(x, verbose=0)[0]
+        best = numpy.argsort(preds)[::-1][:args.number]
+        sys.stdout.write(
+            "%s\n" % " ".join("%s@%.3f" % (ivoc[i], preds[i]) for i in best))
         sys.stdout.flush()
     backend.clear_session()
 
