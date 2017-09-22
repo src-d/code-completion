57bdc9b51b714af67528a660926c91c1488c8469
Vadim Markovtsev <vadim@sourced.tech>
2017-09-22 13:06:17 +0200

    Improve the make.sh tokenizer build script

--- tokenizer/make.sh
+++ tokenizer/make.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
 
+export GOPATH=$(pwd)
+mkdir -p src/github.com/src-d/code-completion/tokenizer/
+ln -fs ../../../../../tokenize src/github.com/src-d/code-completion/tokenizer
 GOOS=darwin GOARCH=amd64 go build -o ../bin/tokenizer_darwin tokenizer.go
 GOOS=windows GOARCH=amd64 go build -o ../bin/tokenizer_win32.exe tokenizer.go
 GOOS=linux GOARCH=amd64 go build -o ../bin/tokenizer_linux tokenizer.go
